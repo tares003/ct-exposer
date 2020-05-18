81999d9a7a8859f9f1a6f287c248e1b630b577c5
tares <stareq13@yahoo.com>
2020-05-18 01:08:51 +0100

    Added support for https://transparencyreport.google.com/https/certificates?hl=en_GB - Support of domains from google DNS history (limited to recent 5)

--- ct-exposer.py
+++ ct-exposer.py
@@ -1,35 +1,93 @@
 #!/usr/bin/env python3
-
+import json
 import sys
 import requests
 import re
 import argparse
 import gevent
+import urllib3
 from gevent import socket
 from gevent.pool import Pool
 
 requests.packages.urllib3.disable_warnings()
-                
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+GOGGLE_HEADERS = {'Host': 'transparencyreport.google.com',
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
+                  'Accept': 'application/json, text/plain, */*',
+                  'Accept-Language': 'en-US,en;q=0.5',
+                  'Accept-Encoding': 'gzip, deflate, br',
+                  'Referer': 'https://transparencyreport.google.com',
+                  "Sec-Fetch-Site": "same-origin",
+                  "Sec-Fetch-Mode": "cors",
+                  "Sec-Fetch-Dest": "empty",
+                  'Connection': 'keep-alive',
+                  "DNT": "1"
+                  }
+
+
+def get_google_cookie():
+    """
+    gets G cookie
+    :return:
+    """
+    global GOGGLE_HEADERS
+    return requests.get("https://transparencyreport.google.com/https/certificates?hl=en_GB", headers=GOGGLE_HEADERS,
+                        verify=False).cookies
+
+
+GOOGLE_COOKIE = get_google_cookie()
+
+
+def get_dns_history_from_google(session_hash):
+    """
+    Gets Domains from google DNS history
+    :return:
+    """
+    global GOGGLE_HEADERS, GOOGLE_COOKIE
+    res = json.loads(requests.get(
+        f"https://transparencyreport.google.com/transparencyreport/api/v3/httpsreport/ct/certbyhash?hash={session_hash}",
+        headers=GOGGLE_HEADERS, verify=False, cookies=GOOGLE_COOKIE).content.decode().lstrip(")]}\'"))
+    # (res)
+    try:
+        domains = res[0][1][7]
+    except TypeError:
+        return []
+    return domains
+
+
 def main(domain, masscanOutput, urlOutput):
     domainsFound = {}
     domainsNotFound = {}
     if (not masscanOutput and not urlOutput):
         print("[+]: Downloading domain list...")
     response = collectResponse(domain)
+    google_domains = collect_response_google(domain)
+
     if (not masscanOutput and not urlOutput):
         print("[+]: Download of domain list complete.")
     domains = collectDomains(response)
+    domains.extend(collect_google_domains(google_domains))
+    pool = Pool(15)
+    greenlet_google = [pool.spawn(get_dns_history_from_google, domain[5]) for domain in
+                       google_domains[:5]]  # Only the top 5 of google dns history
+    pool.join(timeout=1)
+    for g_greenlet in greenlet_google:
+        result = g_greenlet.value
+        if (result):
+            domains.extend(result)
+
+    domains = list(set(domains))
     if (not masscanOutput and not urlOutput):
         print("[+]: Parsed %s domain(s) from list." % len(domains))
-    
-    pool = Pool(15)
+
     greenlets = [pool.spawn(resolve, domain) for domain in domains]
-    pool.join(timeout = 1)
+    pool.join(timeout=1)
     for greenlet in greenlets:
-        result=greenlet.value
+        result = greenlet.value
         if (result):
             for ip in result.values():
-                if ip is not 'none':
+                if ip != 'none':
                     domainsFound.update(result)
                 else:
                     domainsNotFound.update(result)
@@ -44,15 +102,18 @@ def main(domain, masscanOutput, urlOutput):
         print("\n[+]: Domains with no DNS record:")
         printDomains(domainsNotFound)
 
+
 def resolve(domain):
     try:
-        return({domain: socket.gethostbyname(domain)})
+        return ({domain: socket.gethostbyname(domain)})
     except:
-        return({domain: "none"})
+        return ({domain: "none"})
+
 
 def printDomains(domains):
     for domain in sorted(domains):
         print("%s\t%s" % (domains[domain], domain))
+
 
 def printMasscan(domains):
     iplist = set()
@@ -61,24 +122,58 @@ def printMasscan(domains):
     for ip in sorted(iplist):
         print("%s" % (ip))
 
+
 def printUrls(domains):
     for domain in domains:
         print("https://%s" % domain)
 
+
+def collect_response_google(domain, data=[], token=None, depth=5, cookies=None):
+    global GOOGLE_HEADERS, GOOGLE_COOKIE
+    # if not cookies:
+    #     cookies = get_google_cookie()
+
+    if depth == 0:  # pagination limit
+        return data
+
+    if not domain and token:
+        res = requests.get(
+            f"https://transparencyreport.google.com/transparencyreport/api/v3/httpsreport/ct/certsearch/page?p={token}",
+            headers=GOGGLE_HEADERS, verify=False, cookies=GOOGLE_COOKIE)
+    else:
+        domain = domain.replace(" ", "")
+        res = requests.get(
+            f"https://transparencyreport.google.com/transparencyreport/api/v3/httpsreport/ct/certsearch?include_subdomains=true&domain={domain}",
+            headers=GOGGLE_HEADERS, verify=False, cookies=GOOGLE_COOKIE)
+    res_data = json.loads(res.content.decode().lstrip(")]}\'"))
+    if res_data[0][1]:
+        data.extend(res_data[0][1])
+
+    if (res_data[0][1]) and res_data[0][3][1]:  # with token
+        return collect_response_google(None, data, res_data[0][3][1], depth - 1, GOOGLE_COOKIE)
+    else:
+        return data
+
+
 def collectResponse(domain):
     headers = {'Host': 'ctsearch.entrust.com',
-	       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
-	       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	       'Accept-Language': 'en-US,en;q=0.5',
-	       'Accept-Encoding': 'gzip, deflate',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'Accept-Encoding': 'gzip, deflate',
                'Referer': 'https://www.entrust.com/ct-search/',
-	       'Connection': 'close',
-	       'Upgrade-Insecure-Requests': '1',
-	       'Content-Length': '0'}
+               'Connection': 'close',
+               'Upgrade-Insecure-Requests': '1',
+               'Content-Length': '0'}
 
     url = 'https://ctsearch.entrust.com/api/v1/certificates?fields=subjectDN&domain=' + domain + '&includeExpired=true&exactMatch=false&limit=5000'
     response = requests.get(url, headers=headers, verify=False)
     return response
+
+
+def collect_google_domains(domains):
+    return list(set([domain[1] for domain in domains]))
+
 
 def collectDomains(response):
     domains = []
@@ -86,16 +181,19 @@ def collectDomains(response):
     match = re.findall(restring, response.text)
     if match:
         for domain in match:
-            #The following line avoids adding wildcard domains, as they will not resolve.
-            if((domain[0] not in domains) and not (re.search("^\*\.", domain[0]))):
+            # The following line avoids adding wildcard domains, as they will not resolve.
+            if ((domain[0] not in domains) and not (re.search("^\*\.", domain[0]))):
                 domains.append(domain[0])
     return domains
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--domain", type=str, required=True, help="domain to query for CT logs, ex: domain.com")
-    parser.add_argument("-u", "--urls", default=0, action="store_true", help="ouput results with https:// urls for domains that resolve, one per line.")
-    parser.add_argument("-m", "--masscan", default=0, action="store_true", help="output resolved IP address, one per line. Useful for masscan IP list import \"-iL\" format.")
+    parser.add_argument("-u", "--urls", default=0, action="store_true",
+                        help="ouput results with https:// urls for domains that resolve, one per line.")
+    parser.add_argument("-m", "--masscan", default=0, action="store_true",
+                        help="output resolved IP address, one per line. Useful for masscan IP list import \"-iL\" format.")
     args = parser.parse_args()
     main(args.domain, args.masscan, args.urls)
-
+    # collect_response_google("google.com")
